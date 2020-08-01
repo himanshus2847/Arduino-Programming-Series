8f50d7aa1bc2a2ebfdce5ee8ba9ce01c07b3dcc4
Himanshu Sharma <himanshus2847@gmail.com>
2020-08-01 11:37:40 +0100

    Create forloop.ino

---
+++ Tutorial 13 - For Loop/forloop.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+}
+
+void loop()
+{
+  for(int i = 0; i <= 255; i++)
+  {
+    analogWrite(10, i);
+    delay(10);
+  }
+  for(int j = 255; i >= 0; j--)
+  {
+    analogWrite(10, j);
+    delay(10);
+  }
+}
